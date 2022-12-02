29e81eab317f5d4a6ae4a1c273ebc7c35e39fd79
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-02 14:40:45 -0300

    feat: resolve lint

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -9,7 +9,8 @@ def txt_importer(path_file):
             return file.read().splitlines()
 
     except FileNotFoundError:
-        sys.stderr.write('Arquivo ' + path_file +' não encontrado\n')
+        sys.stderr.write('Arquivo ' + path_file + ' não encontrado\n')
+
 
 if __name__ == "__main__":
     txt_importer('statics/arquivo_teste.csv')
