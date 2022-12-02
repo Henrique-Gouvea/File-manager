322a831021d44b89bb4bae88aa53459655cb0c18
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-02 14:28:47 -0300

    feat(file-management): Init process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    print('TTTTTTTTTTTTTTTTTTTTTTTTTTTT')
+    text_list: list = txt_importer(path_file)
+    infoDict: dict = {
+        "name_path_file": path_file,
+        "quantity_words": len(text_list)
+    }
+    instance.enqueue(infoDict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    print('TTTTTTTTTTTTTTTTTTTTTTTTTTTT')
+    print(instance)
+    print('TTTTTTTTTTTTTTTTTTTTTTTTTTTT')
+    if len(instance == 0):
+        return sys.stderr.write('Posição inválida\n')
 
 
 def file_metadata(instance, position):
