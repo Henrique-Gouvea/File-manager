f5f77f1a93ae3d4969f479cd8b1005e964ac88d3
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-08 16:15:11 -0300

    feat(word_search): Create exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
+import re 
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_word_found: list = []
+    for file in instance.queue:
+        ocorrency:list = []
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if len(re.findall(word, line, flags=re.IGNORECASE)) > 0:
+                ocorrency.append({"linha": index + 1})
+        if len(ocorrency) == 0:
+            return []
+        ocorrency_file: dict = {
+            "palavra": word,
+            "arquivo": file['nome_do_arquivo'],
+            "ocorrencias": ocorrency,
+        }
+        list_word_found.append(ocorrency_file)
+    return list_word_found
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+# if __name__ == "__main__":
+#     project = Queue()
+#     process("statics/nome_pedro.txt", project)
+#     word = exists_word("pedro", project)
