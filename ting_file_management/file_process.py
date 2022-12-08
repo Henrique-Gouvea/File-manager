b9eb6026a6b33b74915338a8d12e42468becc930
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-08 09:29:14 -0300

    feat(procees): Create remove and file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,21 +3,33 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
+
+    for file in instance.queue:
+        if file['nome_do_arquivo'] == path_file:
+            return
     text_list: list = txt_importer(path_file)
     infoDict: dict = {
-        "name_path_file": path_file,
-        "quantity_words": len(text_list)
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_list),
+        "linhas_do_arquivo": text_list
     }
+    print(infoDict)
     instance.enqueue(infoDict)
 
 
 def remove(instance):
-    print('TTTTTTTTTTTTTTTTTTTTTTTTTTTT')
-    print(instance)
-    print('TTTTTTTTTTTTTTTTTTTTTTTTTTTT')
-    if len(instance == 0):
-        return sys.stderr.write('Posição inválida\n')
+    if len(instance) == 0:
+        sys.stdout.write("Não há elementos\n")
+        return
+
+    data = instance.dequeue()
+    file_name = data["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {file_name} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        sys.stdout.write(f"{file}")
+    except IndexError:
+        sys.stderr.write("Posição inválida")
